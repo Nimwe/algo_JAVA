101118ec652e8c5bfb7cc1ce17fc23a3e68c1f26
unknown <floallheilly@gmail.com>
2025-04-15 15:25:00 +0200

    Itération sur un tableau

---
+++ src/main/java/algojava/Iteration_tableau.java
@@ -0,0 +1,11 @@
+package algojava;
+
+public class Iteration_tableau {
+    public static void main(String[] args) {
+        
+        double [] array = {12.2, 11.3, 4.4, 2.0};
+            for (int i = 0 ; i < array.length ; i++) {
+                System.out.println(array[i]);
+            }
+    }
+}
