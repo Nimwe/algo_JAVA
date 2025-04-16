dceeea98871018243a259d32040c269ad68158ed
unknown <floallheilly@gmail.com>
2025-04-16 10:51:05 +0200

    Second commit

--- src/main/java/algojava/Iteration_tableau.java
+++ src/main/java/algojava/Iteration_tableau.java
@@ -2,10 +2,10 @@ package algojava;
 
 public class Iteration_tableau {
     public static void main(String[] args) {
-        
-        double [] array = {12.2, 11.3, 4.4, 2.0};
-            for (int i = 0 ; i < array.length ; i++) {
-                System.out.println(array[i]);
-            }
+
+        double[] array = { 12.2, 11.3, 4.4, 2.0 };
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
     }
 }
